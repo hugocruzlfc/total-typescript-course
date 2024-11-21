cfdc8a29403538b2aaf320cefeba32420a950710
Hugo Cruz <hugocruzlfc@gmail.com>
2024-11-21 19:07:16 +0100

    feat: alone section tricks

--- src/sections/autocomplete-string-union-type.ts
+++ src/sections/autocomplete-string-union-type.ts
@@ -7,3 +7,12 @@ type IconProps = {
 const Icon = ({ color }: IconProps) => {
   // ...
 };
+
+// We aren't getting color suggestions
+// <Icon color="primary" />
+
+type IColor2 = "primary" | "secondary" | (string & {});
+
+// <Icon color="primary" />;
+// <Icon color="secondary" />;
+// <Icon color="#fff" />;
