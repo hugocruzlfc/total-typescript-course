45d228694e48f29d1be40e701741be6e22e8d87e
Hugo Cruz <hugocruzlfc@gmail.com>
2025-02-17 18:41:04 +0100

    feat: update section basic essentials

---
+++ src/sections/TypeScript-Pro-Essentials/optional-members-of-tuples.ts
@@ -0,0 +1,24 @@
+import { Equal, Expect } from "@/utils/type-utils";
+
+const goToLocation = (
+  coordinates: [latitude: number, longitude: number, elevation?: number] // [number, number, number?]
+) => {
+  const latitude = coordinates[0];
+  const longitude = coordinates[1];
+  const elevation = coordinates[2];
+
+  // Do something with latitude, longitude, and elevation in here
+
+  type tests = [
+    Expect<Equal<typeof latitude, number>>,
+    Expect<Equal<typeof longitude, number>>,
+    Expect<Equal<typeof elevation, number | undefined>>
+  ];
+};
+
+goToLocation([10, 20]);
+
+// @ts-expect-error string is not assignable to number
+goToLocation([10, "20"]);
+
+goToLocation([10, 20, 30]);
